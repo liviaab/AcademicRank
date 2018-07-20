c012a116022c05a0496c689a60ceda717e549eed
liviaab <liviaab7@gmail.com>
2018-07-19 21:54:16 -0300

    update univ score

--- app/controllers/universities_controller.rb
+++ app/controllers/universities_controller.rb
@@ -8,6 +8,19 @@ class UniversitiesController < ApplicationController
 		@courses = @university.courses 
 	end
 
+	def edit
+		@university = University.find(params[:id])
+	end
+
+	def update
+		@university = University.find(params[:id])
+		if @university.update(uni_params)
+			redirect_to '/'
+		else
+			render 'show'
+		end
+	end
+
 
 	def create
 		@university = University.new(uni_params)
@@ -22,4 +35,5 @@ class UniversitiesController < ApplicationController
 		def uni_params
 			params.require(:university).permit(:university_name, :final_score)
 		end
+
 end
